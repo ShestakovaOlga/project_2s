5561e852bf6c8df98718afff3e5d812ba51cacee
Olga Shestakova <olgashestakova@MacBook-Air.home>
2019-06-18 17:50:59 +0200

    up

--- in.js
+++ in.js
@@ -1,6 +1,17 @@
 import './cardPage.html';
 
+
 const buttonStart = document.querySelector('.start_button');
-buttonStart.addEventListener('click', () => {
-    window.location.href = "/cardPage.html";
-})
+if (buttonStart) {
+    buttonStart.addEventListener('click', () => {
+        window.location.href = "/cardPage.html";
+    })
+}
+
+const nameElement = document.querySelector('#name');
+const nameCardElement = document.querySelector('.name_card');
+if (nameElement) {
+    nameElement.addEventListener('input', () => {
+        nameCardElement.innerHTML = nameElement.value
+    })
+}
