5dcd63884a7462a3bea575dd8bba30dbbe1cb389
Olga Shestakova <olgashestakova@MacBook-Air.home>
2019-06-18 13:27:28 +0200

    Start page

---
+++ in.js
@@ -0,0 +1,6 @@
+import './cardPage.html';
+
+const buttonStart = document.querySelector('.start_button');
+buttonStart.addEventListener('click', () => {
+    window.location.href = "/cardPage.html";
+})
